583a56eea70caf3c4a5cb12dfef7b588825045ce
jefolidev <jefoliveira279@gmail.com>
2024-08-14 07:47:12 -0300

    update/removed the input of email

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -5,23 +5,13 @@ const Main = () => {
   return (
     <div id="main" className="relative w-full h-full ">
       <Image src={backgroundImage} className="w-full px-8 py-2 " alt="" />
-      <section className="absolute inset-0 top-40 left-20 text-white flex flex-col  ">
+      <section className="absolute inset-0 top-52 left-20 text-white flex flex-col  ">
         <h1 className="text-8xl font-giovanni -mb-4 ">Transforme </h1>
         <h1 className="text-9xl font-sequel font-semibold ">SEU VOLÊI</h1>
         <h2 className="text-2xl my-6">
           Descubra quadras próximas, participe de rachas, e <br /> aperfeiçoe
           suas habilidades com treinos especializados.
         </h2>
-        <form className="relative w-[36rem]">
-          <input
-            type="email"
-            placeholder="Insira seu email"
-            className="w-full h-24 rounded-full text-light-fonts px-8 focus:outline-none font-sequel font-semibold"
-          />
-          <button className="absolute w-52 h-20 inset-y-0 right-2 mt-2 rounded-full font-bold bg-light-yellow hover:bg-light-yellowHover">
-            Receber notificações
-          </button>
-        </form>
       </section>
     </div>
   )
